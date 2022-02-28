71c9a45094aaf5990bd7f699152c7d0bff5ed20e
Efim Trivozhenko <name4ef@gmail.com>
2022-02-28 19:02:49 +0700

    added functionality for task

---
+++ tmux_task.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+MIN_COLUMNS_SIZE=82
+DEFAULTWIDTH=60
+POSTFIX=" "
+
+COLUMNS=$1
+
+if (( $COLUMNS <= $MIN_COLUMNS_SIZE )); then
+    DEFAULTWIDTH=32
+    POSTFIX="... "
+fi
+
+echo -n "#[bg=black fg=white]"
+
+if task active &>/dev/null; then
+    TASK=$(task rc.verbose:nothing rc.defaultwidth:$DEFAULTWIDTH tmux\
+        |head -1 |cut -d ' ' -f 2-)
+    echo "${TASK}${POSTFIX}"
+fi
