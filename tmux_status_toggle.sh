6afd58a4bb37ee0c3fdbaec6fbb37c65bda487fc
Efim Trivozhenko <name4ef@gmail.com>
2022-03-03 11:28:03 +0700

    tmux: bugfix of status line toggle

--- tmux_status_toggle.sh
+++ tmux_status_toggle.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 STATUS_CURRENT=$(tmux show -q status)
+if [[ $STATUS_CURRENT == "" ]]; then
+    STATUS_CURRENT=$(tmux show -qg status)
+fi
 
 if [ "$STATUS_CURRENT" == "status on" ]; then
-    #echo $STATUS_CURRENT
     tmux set status off
 elif [ "$STATUS_CURRENT" == "status off" ]; then
     tmux set status on
