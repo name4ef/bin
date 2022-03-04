76235147eef7ffc4e422161e06c14d9d35908121
Efim Trivozhenko <name4ef@gmail.com>
2022-03-04 12:36:31 +0700

    mpc: improved adaptation in dependence of size

--- tmux_mpc.sh
+++ tmux_mpc.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 MIN_COLUMNS_SIZE=82
+MID_COLUMNS_SIZE=144
+MAX_WIDTH=40
 
 COLUMNS=$1
 
@@ -10,7 +12,18 @@ if mpc|grep -q playing; then
         #echo "■ ⏏ "
         echo "▶ "
     else
-        echo "$(mpc current -f '%title% (%date%) %artist%'|sed 's/ *$//') "
+        MPC_LINE=$(mpc current -f '%title% (%date%) %artist%'|sed 's/ *$//')
+        MPC_LINE_SIZE=$(echo $MPC_LINE | wc -m)
+        if (( $COLUMNS <= $MID_COLUMNS_SIZE )); then
+            if (( $MPC_LINE_SIZE > $MAX_WIDTH )); then
+                MPC_LINE_TRIM=$(echo $MPC_LINE | cut -c 1-$MAX_WIDTH)
+                echo "$MPC_LINE_TRIM... "
+            else
+                echo "$MPC_LINE "
+            fi
+        else
+            echo "$MPC_LINE "
+        fi
     fi
 elif mpc|grep -q paused; then
     echo -n "#[bg=black fg=green]"
