37ec7e5005d0c80dbe8283d4c81e01879f93fa44
Efim Trivozhenko <name4ef@gmail.com>
2022-03-04 16:19:45 +0700

    tmux: trimming task string if it needed

--- tmux_task.sh
+++ tmux_task.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 
 MIN_COLUMNS_SIZE=82
-MIN_TASK_SIZE=32
-MAX_WIDTH=60
+MID_COLUMNS_SIZE=144
+MIN_TASK_SIZE=24
+MID_TASK_SIZE=40
 
 COLUMNS=$1
 
 if task active &>/dev/null; then
     echo -n "#[bg=black fg=white]"
-    TASK=$(task rc.verbose:nothing rc.defaultwidth:$MAX_WIDTH tmux\
+    TASK=$(task rc.verbose:nothing tmux\
         | head -1 | sed 's/  */ /g' | cut -d ' ' -f 2-)
-    LENGTH=$(echo -n $TASK | wc -m)
-    if (( $COLUMNS <= $MIN_COLUMNS_SIZE )); then
-        if (( $LENGTH > $MIN_TASK_SIZE)); then
-            echo "$(echo $TASK | cut -c 1-$MIN_TASK_SIZE)... "
+    TASK_SIZE=$(echo -n $TASK | wc -m)
+
+    if (( $COLUMNS <= $MID_COLUMNS_SIZE )); then
+        if (( $TASK_SIZE >= $MID_TASK_SIZE)); then
+            TASK_TIME=$(echo $TASK | rev | cut -d ' ' -f 1 | rev)
+            TRIM_SIZE=$MID_TASK_SIZE
+            if (( $COLUMNS <= $MIN_COLUMNS_SIZE )); then
+                if (( $TASK_SIZE >= $MIN_TASK_SIZE)); then
+                    TRIM_SIZE=$MIN_TASK_SIZE
+                fi
+            fi
+            TASK_TRIM=$(echo $TASK | cut -c 1-$TRIM_SIZE)
+            echo "$TASK_TRIM... $TASK_TIME "
         else
             echo "$TASK "
         fi
